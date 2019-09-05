8e5961997571c1c168043ae57841370ba21d7dc2
wpc <wpc@example.com>
2019-09-05 11:01:10 +0800

    对图表柱状图添加提示内容

--- python_repos.py
+++ python_repos.py
@@ -13,11 +13,17 @@ print("Total repositories:", response_dict['total_count'])
 
 #探索有关仓库的信息
 repo_dicts = response_dict['items']
+print("Number of items:", len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
 	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+	
+	plot_dict = {
+		'value': repo_dict['stargazers_count'],
+		'label': repo_dict['description'],
+		}
+	plot_dicts.append(plot_dict)
 
 #可视化
 my_style = LS('#333366', base_style=LCS)
@@ -36,5 +42,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred python projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
