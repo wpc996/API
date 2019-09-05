d91921acc380befe4b73ca6db268e910e777ecd9
wpc <wpc@example.com>
2019-09-05 09:36:40 +0800

    使用Pygal可视化仓库

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 #执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -11,14 +13,17 @@ print("Total repositories:", response_dict['total_count'])
 
 #探索有关仓库的信息
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-print("\nSelected information about first repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-	print("\nName:", repo_dict['name'])
-	print('Owner:', repo_dict['owner']['login'])
-	print('Stars:', repo_dict['stargazers_count'])
-	print('Repository:', repo_dict['html_url'])
-	print('Created:', repo_dict['created_at'])
-	print('Update:', repo_dict['updated_at'])
-	print('Deprecation:', repo_dict['description'])
+	names.append(repo_dict['name'])
+	stars.append(repo_dict['stargazers_count'])
+
+#可视化
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred python projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
